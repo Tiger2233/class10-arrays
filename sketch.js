202d451267bd0561f88363960971a3d3d7c0b962
Tiger2233 <101088741+Tiger2233@users.noreply.github.com>
2022-03-14 18:31:51 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var marks =[100,95,40,99,50,10,45,20,30] 
+
+var total = marks[0] +marks[1] +marks[2] +marks[3] +marks[4] +marks[5] +marks[6] +marks[7] +marks[8]
+console.log(total)
+
+var avg = total/marks.length
+console.log(avg)
+
+
+
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
